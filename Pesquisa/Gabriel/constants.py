903d4ec2618ae9b626c62ad20abf1f3449aa5d9f
Gabriel Viana <gabrielviana3662@gmail.com>
2024-05-27 09:05:20 -0300

    add body test obj

---
+++ Pesquisa/Gabriel/constants.py
@@ -0,0 +1,25 @@
+MEASUREMENTS = ['weight_kg',  # 'height_cm', 'weight_kg
+                'stature_cm', 
+                'neck_base_girth', 
+                'chest_girth',
+                'waist_girth',
+                'hips_buttock_girth',
+                'shoulder_girth',
+                'thigh_girth',
+                'thigh_low_girth',
+                'calf_girth',
+                'ankle_girth',
+                'forearm_girth',
+                'wrist_girth',
+                'shoulder_length',
+                'sleeveoutseam_length',
+                'forearm_length',
+                'crotchheight_length',
+                'waistback_length',
+                'thigh_length',
+                'chest_depth_length',
+                'head_girth']
+
+
+
+NUM_OF_MEASUREMENTS = len(MEASUREMENTS)
