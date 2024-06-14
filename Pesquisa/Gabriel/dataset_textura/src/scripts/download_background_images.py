ea3697c4d30f07b5a7c38584d9ccb6ccd53396f7
Gabriel Viana <gabrielviana3662@gmail.com>
2024-06-13 22:53:38 -0300

    rework code for downloading images from unsplash api

--- Pesquisa/Gabriel/dataset_textura/src/scripts/download_background_images.py
+++ Pesquisa/Gabriel/dataset_textura/src/scripts/download_background_images.py
@@ -12,28 +12,29 @@ except Exception as e:
     print('You can create one by running the command: `cp .env.example .env`')
     sys.exit(1)
 
-def download_images(query, count, dest_folder, api_key):
+def download_images(query, count,page,dest_folder, api_key):
     os.makedirs(dest_folder, exist_ok=True)
     BASE_URL = 'https://api.unsplash.com'
     ROUTE = 'search/photos'
+    URL = f'{BASE_URL}/{ROUTE}'
     QUERY_PARAMS = {
-        "query": "livingRoom",
-        "per_page": str(count),
-        "secret_key": os.getenv("UNSPLASH_API_KEY")
+        "query": "living_room",
+        "per_page": int(count),
+        "page": int(page),
+        "client_id": os.getenv("UNSPLASH_API_KEY")
     }
-    url = f'{BASE_URL}/{ROUTE}?{"&".join([*QUERY_PARAMS.values()])}'
-    print(url)
+
     try:
-        response = requests.get(url)
-        data = response.json()
-        for i, image in enumerate(data['results']):
-            print(len(data['results']))
-            break
-            image_url = image['urls']['regular']
-            image_response = requests.get(image_url)
-            with open(os.path.join(dest_folder, f'background_{i+1}.jpg'), 'wb') as f:
-                f.write(image_response.content)
-            print(f'Downloaded {i+1}/{count} images')
+        for i in range(1,page+1):
+            QUERY_PARAMS['page'] = i
+            response = requests.get(URL,params=QUERY_PARAMS)
+            data = response.json()
+            for i, image in enumerate(data['results']):
+                image_url = image['urls']['regular']
+                image_response = requests.get(image_url)
+                with open(os.path.join(dest_folder, f'background_{i+1}.jpg'), 'wb') as f:
+                    f.write(image_response.content)
+                print(f'Downloaded {i+1}/{count} images')
     except Exception as e:
         print('Error downloading images:', e)
 
@@ -42,8 +43,9 @@ unsplash_api_key = os.getenv("UNSPLASH_API_KEY")
 
 # Define the search query and number of images you need
 search_query = 'livingroom'
-num_images = 50 # max per hour
+num_images = 30 # max per hour
+page = 2
 destination_folder = os.path.join(ROOT_DIR,'downloads')
 
 # Download the images
-download_images(search_query, num_images, destination_folder, unsplash_api_key)
+download_images(search_query, num_images,page, destination_folder, unsplash_api_key)
