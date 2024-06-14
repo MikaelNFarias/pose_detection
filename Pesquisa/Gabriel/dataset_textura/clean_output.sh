1f3fac88a0dcdb63ea52ebf62b86d45718a843e2
Gabriel Viana <gabrielviana3662@gmail.com>
2024-06-14 13:00:23 -0300

    add bash script to clean output for testing

---
+++ Pesquisa/Gabriel/dataset_textura/clean_output.sh
@@ -0,0 +1,7 @@
+rm data/output/train/annotations/render/*
+rm data/output/train/annotations/measurements/*
+rm data/output/train/annotations/plane/*
+
+rm data/output/train/frontal/*
+rm data/output/train/side/*
+rm data/schemas/train/*
